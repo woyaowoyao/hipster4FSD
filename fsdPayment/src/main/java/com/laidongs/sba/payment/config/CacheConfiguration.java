4f4bc9be3693f2a70d69b4bf608969c03877e6ff
dongshi <laidongs@cn.ibm.com>
2019-10-29 09:17:40 +0800

    payment

--- fsdPayment/src/main/java/com/laidongs/sba/payment/config/CacheConfiguration.java
+++ fsdPayment/src/main/java/com/laidongs/sba/payment/config/CacheConfiguration.java
@@ -40,6 +40,7 @@ public class CacheConfiguration {
     @Bean
     public JCacheManagerCustomizer cacheManagerCustomizer() {
         return cm -> {
+            createCache(cm, com.laidongs.sba.payment.domain.PaymentRecord.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
