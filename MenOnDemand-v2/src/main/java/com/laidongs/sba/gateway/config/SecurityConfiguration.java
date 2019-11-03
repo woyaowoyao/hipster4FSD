0eb553ce05e3fadb1a518e4e15119a4be9c9cc45
dongshi <laidongs@cn.ibm.com>
2019-11-03 22:32:57 +0800

    u

--- MenOnDemand-v2/src/main/java/com/laidongs/sba/gateway/config/SecurityConfiguration.java
+++ MenOnDemand-v2/src/main/java/com/laidongs/sba/gateway/config/SecurityConfiguration.java
@@ -82,6 +82,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/mentor").permitAll()//匿名访问url 清单
+            .antMatchers("/api/account").permitAll()//匿名访问url 清单
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
